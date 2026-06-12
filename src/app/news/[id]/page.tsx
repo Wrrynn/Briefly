90c1c211c44aecb3114914af9e90b5079d102f40
Harris <137796872+SShineee@users.noreply.github.com>
2026-06-12 10:37:38 +0700

    Change text from 'Compro' to 'Briefly'

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -131,7 +131,7 @@ export default function NewsDetailPage() {
                             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-500 opacity-75" />
                             <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-blue-600 shadow-[0_0_8px_rgba(59,130,246,0.8)]" />
                         </div>
-                        <span>Compro</span>
+                        <span>Briefly</span>
                     </Link>
 
                     <div className="flex items-center gap-5">
